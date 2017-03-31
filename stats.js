9e56687e95d35b16531630e291c366ee67bf3fa4
Ben Iofel <iofelben@gmail.com>
2017-03-31 17:45:37 -0400

    replace a var with const

--- stats.js
+++ stats.js
@@ -2,6 +2,7 @@ const request = require('request')
 
 const colors = ["#FFFFFF","#E4E4E4","#888888","#222222","#FFA7D1","#E50000","#E59500","#A06A42","#E5D900","#94E044","#02BE01","#00D3DD","#0083C7","#0000EA","#CF6EE4","#820080"]
 
+// taken from place-base.js
 function decode(e) {
     var i = new Uint8Array(1000 * 1000)
     var s = 0
@@ -27,7 +28,7 @@ module.exports = function () {
             const count = {}
             for (const color of colors)
                 count[color] = 0
-            for (var tile of canvas)
+            for (const tile of canvas)
                 count[colors[tile]]++
 
             resolve(count)
